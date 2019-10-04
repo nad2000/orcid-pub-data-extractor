bdaf1bbc214d28d4f2f6836e9e0bb898f594779f
Rad Cirskis <nad2000@gmail.com>
2019-10-05 00:12:43 +1300

    adde go lang implementation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nad2000/orcid-pub-data-extractor
+
+go 1.13
+
+require github.com/sirupsen/logrus v1.4.2
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type record struct {
+	XMLName            xml.Name // `xml:"employment"`
+	Text               string   `xml:",chardata"`
+	Internal           string   `xml:"internal,attr"`
+	Address            string   `xml:"address,attr"`
+	Email              string   `xml:"email,attr"`
+	History            string   `xml:"history,attr"`
+	Employment         string   `xml:"employment,attr"`
+	Person             string   `xml:"person,attr"`
+	Education          string   `xml:"education,attr"`
+	OtherName          string   `xml:"other-name,attr"`
+	PersonalDetails    string   `xml:"personal-details,attr"`
+	Bulk               string   `xml:"bulk,attr"`
+	Common             string   `xml:"common,attr"`
+	Record             string   `xml:"record,attr"`
+	Keyword            string   `xml:"keyword,attr"`
+	Activities         string   `xml:"activities,attr"`
+	Deprecated         string   `xml:"deprecated,attr"`
+	ExternalIdentifier string   `xml:"external-identifier,attr"`
+	Funding            string   `xml:"funding,attr"`
+	Error              string   `xml:"error,attr"`
+	Preferences        string   `xml:"preferences,attr"`
+	Work               string   `xml:"work,attr"`
+	ResearcherURL      string   `xml:"researcher-url,attr"`
+	PeerReview         string   `xml:"peer-review,attr"`
+	PutCode            string   `xml:"put-code,attr"`
+	Path               string   `xml:"path,attr"`
+	Visibility         string   `xml:"visibility,attr"`
+	CreatedDate        string   `xml:"created-date"`
+	LastModifiedDate   string   `xml:"last-modified-date"`
+	RoleTitle          string   `xml:"role-title"`
+	Organization       struct {
+		Text    string `xml:",chardata"`
+		Name    string `xml:"name"`
+		Address struct {
+			Text    string `xml:",chardata"`
+			City    string `xml:"city"`
+			Region  string `xml:"region"`
+			Country string `xml:"country"`
+		} `xml:"address"`
+	} `xml:"organization"`
+	ConveningOrganization struct {
+		Text    string `xml:",chardata"`
+		Name    string `xml:"name"`
+		Address struct {
+			Text    string `xml:",chardata"`
+			City    string `xml:"city"`
+			Country string `xml:"country"`
+		} `xml:"address"`
+	} `xml:"convening-organization"`
+}
+
+type fileEntry struct {
+	fn      string
+	content []byte
+}
+
+func main() {
+
+	var filename = "/home/rcir178/Data/ORCID-API-2.0_activities_xml_10_2018.tar.gz"
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	zr, err := gzip.NewReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tr := tar.NewReader(zr)
+
+	// fc := make(chan fileEntry, 100)
+	// done := make(chan bool)
+	var wg sync.WaitGroup
+
+	for {
+		h, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+
+		fn := h.Name
+		switch h.Typeflag {
+		case tar.TypeDir:
+			continue
+		case tar.TypeReg:
+			if strings.HasSuffix(fn, ".xml") {
+				recBytes, _ := ioutil.ReadAll(tr)
+				wg.Add(1)
+				go handleFile(fn, recBytes, &wg)
+			}
+		default:
+			log.Infof("%s : %c %s %s\n",
+				"Yikes! Unable to figure out type",
+				h.Typeflag,
+				"in file",
+				fn,
+			)
+		}
+	}
+	wg.Wait()
+}
+
+func handleFile(fn string, content []byte, wg *sync.WaitGroup) {
+	if bytes.Contains(content, []byte(">NZ<")) {
+		var rec record
+		xml.Unmarshal(content, &rec)
+		if rec.Organization.Address.Country == "NZ" || rec.ConveningOrganization.Address.Country == "NZ" {
+			log.Info(fn)
+			err := ioutil.WriteFile(filepath.Base(fn), content, 0644)
+			if err != nil {
+				log.Error(err)
+			}
+		}
+	}
+	wg.Done()
+}
